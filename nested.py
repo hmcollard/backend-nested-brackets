5b25759adc96b76f87088927e099f4aa7321ef1a
Haley Collard <collard.haley20@gmail.com>
2020-07-22 19:06:52 -0400

    started working on bracket checker

--- nested.py
+++ nested.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module docstring: One line description of what your program does.
+Nested Brackets: Determine if a line of code has properly nested brackets.
 """
-__author__ = "???"
+__author__ = "Haley Collard"
 
 import sys
 
 
 def is_nested(line):
     """Validate a single input line for correct nesting"""
-    pass
+    opening_brackets = ['(', '[', '{', '<', '(*']
+    closing_brackets = ['*)', '>', '}', ']', ')']
+    brackets = []
+    for char in line:
+        if char in opening_brackets:
+            brackets.append(char)
+        elif char in closing_brackets:
+            if not brackets:
+                return 'NO'
+            else:
+                brackets.pop()
+    if not brackets:
+        return 'YES'
+    else:
+        return 'NO'
 
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    results = []
+    with open('input.txt', 'r') as f:
+        text = f.readlines()
+        for line in text:
+            result = is_nested(line)
+            results.append(result)
+
+    print(results)
+    with open("output.txt", 'w') as f:
+        f.write(results)
 
 
 if __name__ == '__main__':
